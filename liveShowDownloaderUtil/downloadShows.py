d6cabacb67ef1b9d3e7a704efc1fcdfcd60eb824
Tejasv Kaushik <tejasv2003@gmail.com>
2024-01-17 17:49:59 +0530

    can now download both audi and video files in seperate subdirectories

--- liveShowDownloaderUtil/downloadShows.py
+++ liveShowDownloaderUtil/downloadShows.py
@@ -1,12 +1,28 @@
 import pytube
 from pytube import YouTube
+import os
 
 def downloadShow(link, title, path):
     try:
         yt = YouTube(link)
-        t = yt.streams.filter(only_audio=True)
-        t[0].download(path, filename=title + ".mp3")
-        print("Downloaded " + title)
+        t = yt.streams
+
+        videoPath = path + '\\video'
+        print(videoPath)
+        if not os.path.exists(videoPath):
+            os.makedirs(videoPath)
+
+        t[0].download(videoPath, filename=title + ".mp4")
+        print("Video Downloaded " + title)
+
+        audioPath = path + '\\audio'
+        print(audioPath)
+        if not os.path.exists(audioPath):
+            os.makedirs(audioPath)
+        
+        t.filter(only_audio=True)
+        t[0].download(audioPath, filename=title + ".mp3")
+        print("Audio Downloaded " + title)
     except:
         print("Failed to download " + title)
         pass
--- liveShowDownloaderUtil/getShowLinks.py
+++ liveShowDownloaderUtil/getShowLinks.py
@@ -3,15 +3,22 @@ from pytube import YouTube
 from pytube import Search
 
 def getShowLinks(name):
-    links = []
-    titles = []
-    searchResults = Search(name + " live")
-    searchResults.results
-    searchResults.get_next_results()
+    try: 
+        links = []
+        titles = []
+        searchResults = Search(name + " live")
+        print("found search results")
+        searchResults.results
+        searchResults.get_next_results()
+        print("got next results")
 
-    for i in searchResults.results:
-        if(i.length > 3000):
-            links.append(i.watch_url)
-            titles.append(i.title)
+        for i in searchResults.results:
+            if(i.length > 3000):
+                links.append(i.watch_url)
+                titles.append(i.title)
+                print("Added")
 
-    return links, titles
+        return links, titles
+    except:
+        print("Failed to get links")
+        pass
