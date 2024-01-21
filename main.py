7f2ed356c1304e307a2d76e385645abd123cddc0
Tejasv Kaushik <tejasv2003@gmail.com>
2024-01-21 19:38:53 +0530

    Fetches links and titles but in a better way hehe

--- main.py
+++ main.py
@@ -4,38 +4,86 @@ import path as path
 import os
 import tkinter as tk
 import customtkinter as ctk
+import time
 
 # finishLabel = None
 
-def findLiveShows(artist, finishLabel):
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("green")
+
+app = ctk.CTk()
+app.title("Live Show Downloader")
+app.geometry("720x480")
+
+def findLiveShows(artist, finishLabel, linksAndTitles):
     # global finishLabel
     links, titles = getShowLinks(artist)
-    searchResults = f"Live shows found: {len(links)}"
+
+    for link, title in zip(links, titles):
+        linksAndTitles[title] = link
+
+    searchResults = f"Found {len(links)} shows for {artist}"
     finishLabel.configure(text=searchResults)
 
-def main():
-    ctk.set_appearance_mode("dark")
-    ctk.set_default_color_theme("green")
+    if linksAndTitles:
+        linksAndTitlesFrame = ctk.CTkScrollableFrame(app)
+        linksAndTitlesFrame.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
 
-    app = ctk.CTk()
-    app.title("Live Show Downloader")
-    app.geometry("720x480")
+        linksAndTitlesLabel = ctk.CTkLabel(linksAndTitlesFrame, text=artist, font=("Arial", 20))
+        linksAndTitlesLabel.pack(fill=tk.X, expand=True)
+
+        linksAndTitlesListbox = ctk.CTkTextbox(linksAndTitlesFrame, font=("Arial", 20))
+        linksAndTitlesListbox.pack(fill=tk.BOTH, expand=True)
+
+        for title, link in linksAndTitles.items():
+            linksAndTitlesListbox.insert(tk.END, title)
+            linksAndTitlesListbox.insert(tk.END, "\n")
+
+
+
+
+def main():
+    
 
     title = ctk.CTkLabel(app, text="Enter an artist or a band", font=("Arial", 20))
-    title.pack(padx=10, pady=10, fill=tk.X, expand=True)
+    title.pack(padx=10, pady=10)
 
     # artist input
     artist = tk.StringVar()
     link = ctk.CTkEntry(app, textvariable=artist, font=("Arial", 20))
-    link.pack(padx=10, pady=10, fill=tk.X, expand=True)
+    link.pack(padx=10, pady=10, fill=tk.X)
+
+    # dict to store links and titles
+    linksAndTitles = {}
 
     # finish label
     finishLabel = ctk.CTkLabel(app, text="", font=("Arial", 20))
     finishLabel.pack()
 
     # find live shows button
-    findLiveShowsButton = ctk.CTkButton(app, text="Find Live Shows", font=("Arial", 20), command=lambda: findLiveShows(artist.get(), finishLabel))
-    findLiveShowsButton.pack(padx=10, pady=10, fill=tk.X, expand=True)
+    findLiveShowsButton = ctk.CTkButton(app, text="Find Live Shows", font=("Arial", 20), command=lambda: findLiveShows(artist.get(), finishLabel, linksAndTitles={}))
+    findLiveShowsButton.pack(padx=10, pady=5)
+
+    # links and titles
+    
+    
+
+
+    # if linksAndTitles:
+    #     # display links and titles
+    #     linksAndTitlesFrame = ctk.CTkFrame(app)
+    #     linksAndTitlesFrame.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+
+    #     linksAndTitlesLabel = ctk.CTkLabel(linksAndTitlesFrame, text="Links and Titles", font=("Arial", 20))
+    #     linksAndTitlesLabel.pack(fill=tk.X, expand=True)
+
+    #     linksAndTitlesListbox = ctk.CTkTextbox(linksAndTitlesFrame, font=("Arial", 20))
+    #     linksAndTitlesListbox.pack(fill=tk.BOTH, expand=True)
+
+    #     for title, link in linksAndTitles.items():
+    #         linksAndTitlesListbox.insert(tk.END, title)
+
+    
 
     # print("Starting the liveShowDownloader")
     # artist = input("Enter the name of the artist: ")
