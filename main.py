25aa964ceb0e71de738ca5fea07993f447dd9200
Tejasv Kaushik <tejasv2003@gmail.com>
2024-01-21 18:53:07 +0530

    Added a basic homepage GUI

--- main.py
+++ main.py
@@ -2,24 +2,61 @@ from liveShowDownloaderUtil.getShowLinks import getShowLinks
 from liveShowDownloaderUtil.downloadShows import downloadShow
 import path as path
 import os
+import tkinter as tk
+import customtkinter as ctk
+
+# finishLabel = None
+
+def findLiveShows(artist, finishLabel):
+    # global finishLabel
+    links, titles = getShowLinks(artist)
+    searchResults = f"Live shows found: {len(links)}"
+    finishLabel.configure(text=searchResults)
 
 def main():
-    print("Starting the liveShowDownloader")
-    artist = input("Enter the name of the artist: ")
-    links, titles = getShowLinks(artist)
-    print(len(links))
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("green")
+
+    app = ctk.CTk()
+    app.title("Live Show Downloader")
+    app.geometry("720x480")
+
+    title = ctk.CTkLabel(app, text="Enter an artist or a band", font=("Arial", 20))
+    title.pack(padx=10, pady=10, fill=tk.X, expand=True)
+
+    # artist input
+    artist = tk.StringVar()
+    link = ctk.CTkEntry(app, textvariable=artist, font=("Arial", 20))
+    link.pack(padx=10, pady=10, fill=tk.X, expand=True)
+
+    # finish label
+    finishLabel = ctk.CTkLabel(app, text="", font=("Arial", 20))
+    finishLabel.pack()
+
+    # find live shows button
+    findLiveShowsButton = ctk.CTkButton(app, text="Find Live Shows", font=("Arial", 20), command=lambda: findLiveShows(artist.get(), finishLabel))
+    findLiveShowsButton.pack(padx=10, pady=10, fill=tk.X, expand=True)
+
+    # print("Starting the liveShowDownloader")
+    # artist = input("Enter the name of the artist: ")
+    
 
     # newPath = r'D:\Music\LiveConcerts\{}'.format(artist)
-    basePath = path.BASE_PATH
-    newPath = basePath + artist
-    if not os.path.exists(newPath):
-        os.makedirs(newPath)
-    print(newPath)
+    # basePath = path.BASE_PATH
+    # newPath = basePath + artist
+    # if not os.path.exists(newPath):
+    #     os.makedirs(newPath)
+    # print(newPath)
 
-    # downloadShow(links[0], titles[0], newPath)
+    # # downloadShow(links[0], titles[0], newPath)
 
-    for link, title in zip(links, titles):
-        downloadShow(link, title, newPath)
+
+    # for link, title in zip(links, titles):
+    #     downloadShow(link, title, newPath)
+
+
+    # run app
+    app.mainloop()
 
 if __name__ == "__main__":
     main()
